47615e1dd188ab8215f2e1358b49b144433aeef1
Sagar <sagard.sy@gmail.com>
2026-06-27 02:42:41 +0530

    finalize blogging app project

--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const path = require("path");
 const router = Router();
 const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -20,6 +21,27 @@ router.get("/add-new", (req, res) => {
   return res.render("addBlog", {
     user: req.user, // display nav bar in blog page also
   });
+});
+
+router.get("/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments = await Comment.find({ blogId: req.params.id }).populate(
+    "createdBy",
+  );
+  return res.render("blog", {
+    user: req.user,
+    blog,
+    comments,
+  });
+});
+
+router.post("/comment/:blogId", async (req, res) => {
+  await Comment.create({
+    content: req.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
 });
 
 router.post("/", upload.single("coverImage"), async (req, res) => {
